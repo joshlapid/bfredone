7c5a2646c8e3592339440e16a1e6a9b87479405b
Joshua Lapid <lapidjoshua@yahoo.com>
2016-09-15 18:58:51 -0700

    carousel working

--- js/main.js
+++ js/main.js
@@ -16,4 +16,22 @@ $(document).ready(function() {
 
   });
 
+  var owl = $("#owl-demo2");
+
+  owl.owlCarousel({
+
+      itemsCustom : [
+        [0, 2],
+        [450, 4],
+        [600, 4],
+        [700, 4],
+        [1000, 4],
+        [1200, 4],
+        [1400, 4],
+        [1600, 4]
+      ],
+      navigation : true
+
+  });
+
 });
